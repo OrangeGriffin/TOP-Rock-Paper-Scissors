5be1b2fa0c1fcd370f69609fa6cdb5173e3683e2
OrangeGriffin <76541687+OrangeGriffin@users.noreply.github.com>
2023-03-28 14:27:30 -0500

    Add function for getting player choice

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function getComputerChoice() {
-    // Return a random number from 1 to 3, which will then be assigned
-    // to a rock, paper, or scissors value
-    const computerChoice = Math.floor(Math.random() * 3) +1 
-    if (computerChoice === 1) {
-        return 'Rock'
-    } else if (computerChoice === 2) {
-        return 'Paper'
-    } else {
-        return 'Scissors'
-    }
+  // Return a random number from 1 to 3, which will then be assigned
+  // to a rock, paper, or scissors value
+  const computerChoice = Math.floor(Math.random() * 3) + 1;
+  if (computerChoice === 1) {
+    return "rock";
+  } else if (computerChoice === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+
+// Provide prompt for user to enter a choice. Convert entry to all lowercase.
+function getPlayerChoice() {
+  let playerChoice = prompt("Rock, Paper, or Scissors?");
+  playerChoice = playerChoice.toLocaleLowerCase();
+  return playerChoice
 }
